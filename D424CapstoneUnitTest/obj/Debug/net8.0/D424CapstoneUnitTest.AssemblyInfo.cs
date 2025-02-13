0e4119582025ab295371d63100ad94ad7f326542
DubbedDetective <155699317+DubbedDetective@users.noreply.github.com>
2025-02-13 15:20:39 -0600

    Update code

--- D424CapstoneUnitTest/obj/Debug/net8.0/D424CapstoneUnitTest.AssemblyInfo.cs
+++ D424CapstoneUnitTest/obj/Debug/net8.0/D424CapstoneUnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("D424CapstoneUnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf30273b53f68485585bc3bf5b15ca01b067aa62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b9f1a76f3c8d2a475c49dd65f750ce337c2363")]
 [assembly: System.Reflection.AssemblyProductAttribute("D424CapstoneUnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("D424CapstoneUnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
